a9b6ffecc771e127216a1a8799522ad3b0e22ef7
jfreeman <james.perfectserve@gmail.com>
2018-11-04 13:50:15 -0500

    updating to public

--- scripts/copy-to-dist.js
+++ scripts/copy-to-dist.js
@@ -4,13 +4,18 @@ const path = require('path');
 const distPath = path.normalize(`${__dirname}/../dist/`);
 const packageJsonPath = path.normalize(`${__dirname}/../package.json`);
 const indexJsPath = path.normalize(`${__dirname}/../index.js`);
+const readmePath = path.normalize(`${__dirname}/../README.md`);
 
 
 try {
     fs.copySync(packageJsonPath, distPath+path.basename(packageJsonPath));
     console.log(`Copied ${packageJsonPath} => ${distPath+path.basename(packageJsonPath)}`);
+
     fs.copySync(indexJsPath, distPath+path.basename(indexJsPath));
     console.log(`Copied ${indexJsPath} => ${distPath+path.basename(indexJsPath)}`);
+
+    fs.copySync(readmePath, distPath+path.basename(readmePath));
+    console.log(`Copied ${readmePath} => ${distPath+path.basename(readmePath)}`);
 } catch (err) {
     console.error(err);
 }
